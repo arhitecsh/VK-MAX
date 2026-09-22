8db6ed3a2c033f67a73f5305919f98e4119f27a4
Arsenij_V <gogologo123456gogo@gmail.com>
2026-09-22 21:33:27 +0300

    feat: add icons to the application

---
+++ src/components/Icons/Icons.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../content.png';
+import icon from '../../logo.png';
+
+const Icons = () => {
+  return (
+    <div>
+      <img src={logo} alt="Logo" />
+      <img src={icon} alt="Icon" />
+    </div>
+  );
+};
+
+export default Icons;
